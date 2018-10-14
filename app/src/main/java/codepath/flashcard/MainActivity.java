4f8ce2856a36a3a05dd31255d4d4a171f028e981
Sanderhelleso <sanderhelleso@hotmail.com>
2018-10-13 19:05:03 -0700

    can now see what option is correct and false

--- app/src/main/java/codepath/flashcard/MainActivity.java
+++ app/src/main/java/codepath/flashcard/MainActivity.java
@@ -5,6 +5,9 @@ import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,10 +15,15 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import codepath.flashcard.classes.Question;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int questionIndex = 1;
+    private ArrayList<Question> questions = Question.questions;
+    private Question currentQuestion =  questions.get(questionIndex);
 
 
     @Override
@@ -29,5 +37,57 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setQuestion();
+    }
+
+
+    private void setQuestion() {
+
+        // main question
+        final TextView question = (TextView)findViewById(R.id.question);
+        question.setText(currentQuestion.getQuestion());
+
+        // set options
+        setQuestionOptions();
+
+
+    }
+
+    private void setQuestionOptions() {
+
+        // question options
+        ArrayList<Button> options = new ArrayList<Button>();
+        options.add((Button)findViewById(R.id.option1));
+        options.add((Button)findViewById(R.id.option2));
+        options.add((Button)findViewById(R.id.option3));
+
+        int optionIndex = 0;
+        for (String option : currentQuestion.getOptions()) {
+            final Button optionBtn = options.get(optionIndex);
+            optionBtn.setText(option);
+            optionIndex++;
+
+            // add events to each option
+            optionBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    selectOption(optionBtn);
+                }
+            });
+        }
+    }
+
+    private void selectOption(Button selectedOption) {
+
+        // check if selected option is correct
+        if (getResources().getResourceEntryName(selectedOption.getId()).equals(currentQuestion.getCorrect())) {
+            Log.e("CORRECT", "YES");
+        }
+
+        // handle error
+        else {
+
+        }
     }
 }
